1c9486dce29815c7388eaf4a2c1489d52316dae2
William Rowell <wrowell@pacificbiosciences.com>
2017-08-10 11:53:33 -0700

    change output to match usage

--- targeted-sequel-phasing.sh
+++ targeted-sequel-phasing.sh
@@ -1,66 +1,68 @@
 #!/bin/bash
 
-#source /mnt/software/Modules/current/init/bash
-#module load smrtanalysis/mainline
-#module load mummer
+#dependencies: smrtanalysis/mainline, egrep, samtools
 
 CCSBAM=$1
 SUBREADSBAM=$2
-SCRAPSBAM=$3
-ROINAME=$4
-CHROM=$5
-START=$6
-END=$7
-REF=$8
+ROINAME=$3
+CHROM=$4
+START=$5
+END=$6
+REF=$7
+SUBREADSALIGNED=$8
 
-echo "creating directory $ROINAME to store output"
+echo "creating directory ROINAME to store output"
 echo "--------------------------------------------------"
-mkdir $ROINAME
-cd $ROINAME
-
-echo "subsetting reference around ROI"
-echo "--------------------------------------------------"
-python ../faidx.zip $REF $CHROM:$START-$END > ref.subset.fasta
-samtools faidx ref.subset.fasta
+mkdir "$ROINAME"
+cd "$ROINAME"
 
 echo "subsetting CCSBAM for ROI"
 echo "--------------------------------------------------"
-samtools view -b $CCSBAM $CHROM:$START-$END > subset.bam
+samtools view -b "$CCSBAM" ${CHROM}:${START}-${END} > subset.bam
 # subset.bam will include all CCS reads within ROI
 
 echo "phasing CCSBAM around ROI"
 echo "--------------------------------------------------"
-samtools calmd -AEur subset.bam $REF | samtools phase -b phase - > phase.out
+samtools calmd -AEur subset.bam "${REF}" | samtools phase -b phase - > phase.out
 # phase.0.bam and phase.1.bam are phased CCS reads
+python ../phaseout2bed.py phase.out
+# creates phase.bed file containing phase sets (phased blocks) as features
 
 for PHASE in 0 1; do
-	# generate a comma-separated list of all ZMW from QNAME in CCSBAM
+	# generate a comma-separated list of all read prefixes from QNAME in CCSBAM
 	# for example:
 	# QNAME = m54026_161028_224529/4260379/0_3848
-	# ZMW = 4260379
+	# prefix = m54026_161028_224529/4260379
 	echo "generating a list of subreads corresponding to phase $PHASE"
 	echo "--------------------------------------------------"
-	READLIST=`samtools view phase.$PHASE.bam | awk '{ print $1 }' | cut -f1 | cut -d'/' -f2 | sort | uniq | paste -sd','`
+	# retain headers in whitelist
+	echo "^@" > whitelist.txt
+	# create whitelist of reads
+	samtools view phase.$PHASE.bam | \
+		# print line
+		awk '{ print $1 }' | \
+		# cut the first field (read name)
+		cut -f1 | \
+		# cut the first two parts of the field name (movie and zmw)
+		cut -d'/' -f1-2 | \
+		# save unique movie/zmw names to the whitelist
+		xargs -I "%" echo %/ | \
+		sort | uniq >> whitelist.$PHASE.txt
 
-	echo "collecting subreads corresponding to phase $PHASE"
+	echo "filtering reads corresponding to phase $PHASE and barcode $BCFINDEX,$BCRINDEX"
 	echo "--------------------------------------------------"
-	# phase.$PHASE.subreads.bam, phase.$PHASE.scraps.bam, and associated files are produced
-	bam2bam --whitelistZmwNum=$READLIST $SUBREADSBAM $SCRAPSBAM -o phase.$PHASE
+	samtools view -h "${SUBREADSBAM}" | egrep -f whitelist.$PHASE.txt | samtools view -bS - > phase.$PHASE.subreads.bam
 
+if [ "${SUBREADSALIGNED}" != "True" ]; then
 	echo "aligning subreads corresponding to phase $PHASE"
 	echo "--------------------------------------------------"
 	# phase.$PHASE.aligned.bam is produced
-	pbalign phase.$PHASE.subreads.bam ref.subset.fasta phase.$PHASE.aligned.bam
+	pbalign phase.$PHASE.subreads.bam ${REF} phase.$PHASE.aligned.bam
+fi
 
 	echo "calling variants for phase $PHASE"
 	echo "--------------------------------------------------"
 	# phase.$PHASE.consensus.fasta and phase.$PHASE.consensus.gff are produced
-	arrow -r ref.subset.fasta -o phase.$PHASE.consensus.fasta phase.$PHASE.aligned.bam
-	arrow -r ref.subset.fasta -o phase.$PHASE.consensus.gff phase.$PHASE.aligned.bam
+	arrow -r ref.subset.fasta -o phase.$PHASE.consensus.fasta -o phase.$PHASE.vcf --referenceWindow ${CHROM}:${START}-${END} phase.$PHASE.aligned.bam
 done
 
-echo "using nucmer to show snp differences between the two phases"
-echo "--------------------------------------------------"
-# out.delta and nucmer.snps are produced
-nucmer phase.0.consensus.fasta phase.1.consensus.fasta
-show-snps -x 10 out.delta > nucmer.snps
